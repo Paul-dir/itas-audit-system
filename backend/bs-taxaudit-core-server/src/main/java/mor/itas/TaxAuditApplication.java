bed29d7fcf117c3089f9527fa0d603539b7d281d
Yoseph Tadesse <Yoseph.Tadesse@haramaya.edu.et>
2026-09-08 14:44:42 +0300

    Add backend seeders and observability

--- backend/bs-taxaudit-core-server/src/main/java/mor/itas/TaxAuditApplication.java
+++ backend/bs-taxaudit-core-server/src/main/java/mor/itas/TaxAuditApplication.java
@@ -2,8 +2,10 @@ package mor.itas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TaxAuditApplication {
     public static void main(String[] args) {
         SpringApplication.run(TaxAuditApplication.class, args);
